9927826b00727282915a1e08e85dcfbe863a17aa
camila de vilas boas <camiladevilasboas@gmail.com>
2025-06-04 18:07:23 -0300

    STYLE SWITCHER

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,11 +27,41 @@ navToggle.addEventListener('click', () => {
 /*=============== EMAIL JS ===============*/
 
 /*=============== STYLE SWITCHER ===============*/
+const styleSwitcher = document.getElementById('style-switcher'),
+  switcherToggle = document.getElementById('switcher-toggle'),
+  switcherClose = document.getElementById('switcher-close');
 
 /* Switcher show */
+switcherToggle.addEventListener('click', () => {
+  styleSwitcher.classList.add('show-switcher');
+});
 
 /* Switcher hidden */
+switcherClose.addEventListener('click', () => {
+  styleSwitcher.classList.remove('show-switcher');
+});
 
 /*=============== THEME COLORS ===============*/
+const colors = document.querySelectorAll('.style-switcher-color');
+
+colors.forEach((color) => {
+  color.onclick = () => {
+    const activeColor = color.style.getPropertyValue('--hue');
+
+    colors.forEach((c) => c.classList.remove('active-color'));
+    color.classList.add('active-color');
+
+    document.documentElement.style.setProperty('--hue', activeColor);
+  };
+});
 
 /*=============== LIGHT/DARK MODE ===============*/
+let currentTheme = 'light';
+document.body.className = currentTheme;
+
+document.querySelectorAll('input[name="body-theme"]').forEach((input) => {
+  input.addEventListener('change', () => {
+    currentTheme = input.value;
+    document.body.className = currentTheme;
+  });
+});
