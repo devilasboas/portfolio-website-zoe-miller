4dc029c84669c6a22c5714d1494279c01ee23969
camila de vilas boas <camiladevilasboas@gmail.com>
2025-06-06 20:59:05 -0300

    resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,6 +56,28 @@ function activeWork() {
 
 linkWork.forEach((a) => a.addEventListener('click', activeWork));
 /*=============== RESUME ===============*/
+const accordionItems = document.querySelectorAll('.resume-item');
+
+accordionItems.forEach((item) => {
+  const header = item.querySelector('.resume-header'),
+    content = item.querySelector('.resume-content'),
+    icon = item.querySelector('.resume-icon i');
+
+  header.addEventListener('click', () => {
+    const isOpen = item.classList.toggle('accordion-open');
+
+    content.style.height = isOpen ? content.scrollHeight + 'px' : '0';
+    icon.className = isOpen ? 'ri-subtract-line' : 'ri-add-line';
+
+    accordionItems.forEach((otherItem) => {
+      if (otherItem !== item && otherItem.classList.contains('accordion-open')) {
+        otherItem.querySelector('.resume-content').style.height = '0';
+        otherItem.querySelector('.resume-icon i').classList = 'ri-add-line';
+        otherItem.classList.remove('accordion-open');
+      }
+    });
+  });
+});
 
 /*=============== TESTIMONIALS SWIPER ===============*/
 
